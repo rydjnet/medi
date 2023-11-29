87d70e7f6637f909e8a1ac21c1e605b35fd1ef96
Dmitry Ovchinnikov <rydjnet@gmail.com>
2023-11-29 17:05:02 +0300

    Learning to work with DB

--- main.go
+++ main.go
@@ -54,6 +54,27 @@ func (e EnvironmentConfigProvider) GetPostgresConfig() PgConfig {
 	}
 }
 
+// INSERT INTO public.medi (id, name, docmed) VALUES (uuid_generate_v4(), 'new', false);
+func NewMedi(db *sql.DB, name string, docma bool) bool {
+	sqlStatement := "INSERT INTO public.medi (id, name, docmed) VALUES (uuid_generate_v4(), $1, $2)"
+	_, err := db.Exec(sqlStatement, name, docma)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func CheckMedi(db *sql.DB, name string) (string, bool) {
+	var name_res string
+	var r bool
+	sqlStatement := "SELECT name,docmed FROM public.medi where name=$1"
+	err := db.QueryRow(sqlStatement, name).Scan(&name_res, &r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return name_res, r
+}
+
 func main() {
 	configProvider := EnvironmentConfigProvider{}
 
@@ -64,7 +85,6 @@ func main() {
 	// open database
 	db, err := sql.Open("postgres", psqlconn)
 	CheckError(err)
-
 	// close database
 	defer db.Close()
 
@@ -73,6 +93,10 @@ func main() {
 	CheckError(err)
 
 	fmt.Println("Connected!")
+	p := false
+	NewMedi(db, "test", p)
+	res1, res2 := CheckMedi(db, "test")
+	fmt.Println(res1, res2)
 }
 
 func CheckError(err error) {
